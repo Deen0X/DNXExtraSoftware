a393bec3b0d7a6cc5cca8d5b53a3a93be10e2a7c
Deen0X <deen0x.eol@gmail.com>
2023-12-12 22:57:55 +0100

    Update UpdateExtras.cmd

--- _GetExtras/UpdateExtras.cmd
+++ _GetExtras/UpdateExtras.cmd
@@ -5,10 +5,6 @@ cd "%~dp0"
 set myF=C:\DNXSoftware\Extras\_GetExtras\
 set myZIP=%TEMP%\DNXWIN.zip
 
-echo option=%1
-pause
-
-if "_%1"=="_/HELLO" goto Hello
 if "_%1"=="_/UPDATE" goto updateMe
 if "_%1"=="_/ENDUPDATE" goto StartProcess
 
@@ -19,15 +15,8 @@ rmdir /S /Q %Temp%\DNXWin-main
 wget -O %myZIP% https://github.com/Deen0X/DNXWIN/archive/refs/heads/main.zip
 cd %temp%
 7z x -y %myZIP% * %Temp%
-::"_Check for Updated Extras.cmd" /UPDATE
 cd "%TEMP%\DNXWin-main\_GetExtras"
-echo current dir="%TEMP%\DNXWin-main\_GetExtras"
-echo im going to run updateextras on temp
-pause
 UpdateExtras.cmd /UPDATE
-echo si estoy viendo esto, entonces no se ha pasado el control a la línea anterior (id 1)
-pause
-::del /Q UpdateExtras.cmd
 goto endScript
 
 :updateMe
@@ -36,22 +25,13 @@ goto endScript
 del /S /Q C:\DNXSoftware\Extras\_GetExtras
 xcopy /E /Y %Temp%\DNXWin-main\_GetExtras C:\DNXSoftware\Extras\_GetExtras\
 cd %myF%
-echo myF=%myF%
 ren UpdateExtras.cmd "_Check for Updated Extras.cmd"
 "_Check for Updated Extras.cmd" /ENDUPDATE
-echo si estoy viendo esto, entonces no se ha pasado el control a la línea anterior (id 2)
-pause
-goto endScript
-
-:Hello
-echo Hello my friend!
-pause
 goto endScript
 
 :StartProcess
 echo Now, this script must navigate into all directories and run each subscript, for adding new options to _Get Extras menu.
 pause
-
 
 :endScript
 
