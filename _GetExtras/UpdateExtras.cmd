71e1379cce4dbe164cb85e4fa4560121e920dfcf
Deen0X <deen0x.eol@gmail.com>
2023-12-13 12:02:02 +0100

    Update UpdateExtras.cmd

--- _GetExtras/UpdateExtras.cmd
+++ _GetExtras/UpdateExtras.cmd
@@ -73,7 +73,6 @@ echo Start Extras Update
 echo Ckecking new entries
 echo =============================================================================================================
 rmdir /S /Q "%TEMP%\DNXWin-main"
-pause
 FOR /D %%G in (%myF%*) do call :checkFolder "%%G"
 goto endScript
 
