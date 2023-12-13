bce6d5e37219ebbab613ce82d12b0f387e310da3
Deen0X <deen0x.eol@gmail.com>
2023-12-13 14:11:58 +0100

    Update UpdateExtras.cmd

--- _GetExtras/UpdateExtras.cmd
+++ _GetExtras/UpdateExtras.cmd
@@ -46,8 +46,7 @@ if "_%1"=="_/ENDUPDATE" goto StartProcess
 rmdir /S /Q %Temp%\DNXWin-main
 wget -O %myZIP% https://github.com/Deen0X/DNXWIN/archive/refs/heads/main.zip
 cd %temp%
-start "Unzip" /WAIT powershell -command "Expand-Archive %MyZIP% %Temp%"
-pause
+start "Unzip" /WAIT /MIN powershell -command "Expand-Archive %MyZIP% %Temp%"
 ::7z x -y %myZIP% * %Temp%
 cd "%TEMP%\DNXWin-main\_GetExtras"
 UpdateExtras.cmd /UPDATE
