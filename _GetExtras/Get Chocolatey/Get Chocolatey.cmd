f40b966111d24893852f108ec6ac5b6a83f23435
Deen0X <deen0x.eol@gmail.com>
2023-12-13 01:19:26 +0100

    Update Get Chocolatey.cmd

--- _GetExtras/Get Chocolatey/Get Chocolatey.cmd
+++ _GetExtras/Get Chocolatey/Get Chocolatey.cmd
@@ -4,6 +4,28 @@ pushd "%~dp0"
 set mySN=%~n0
 setlocal enableextensions disabledelayedexpansion
 
+::================================================================================================= Check for permissions
+:getAdminPriv
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\\getadmin.vbs"
+    set params = %*:"="
+    ::echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c  %~s0 %1 %2 %3", "", "runas", 1 >> "%temp%\\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\\getadmin.vbs"
+    exit /B
+::================================================================================================= end checking admin
+
+:gotAdmin
+
 :: ALL scripts must start with "GET " text
 ::################################################################################################# Config variable for checking if Extra is installed
 set "checkEXTRA=C:\ProgramData\chocolatey\choco.exe"
@@ -14,32 +36,10 @@ set xDESCEXTRA=Chocolatey is a machine-level, command-line package manager and i
 if "_%1"=="_/GETEXTRA" goto getExtra
 goto addEntry
 
-::================================================================================================= Check for permissions
-:getAdminPriv
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "C:\Temp\getadmin.vbs"
-    set params = %*:"="
-    ::echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "cmd.exe", "/c  %~s0 %1 %2 %3", "", "runas", 1 >> "C:\Temp\getadmin.vbs"
-
-    "C:\Temp\getadmin.vbs"
-    del "C:\Temp\getadmin.vbs"
-    exit /B
-::================================================================================================= end checking admin
-
-:gotAdmin
-exit /b
 
 :addEntry
 ::================================================================================================= Check if EXTRA exist
+echo "%checkEXTRA%"
 if exist "%checkEXTRA%" goto removeEntry
 ::------------------------------------------------------------------------------------------------- Add Entry if not exist
 echo --- addEntry
@@ -61,8 +61,7 @@ goto endScript
 ::================================================================================================= Start Main Script for Get EXTRA
 :getExtra
 echo --- getExtra
-call :getAdminPriv
-
+::call :getAdminPriv
 cls
 echo APP Name    : %mySN%
 echo Description : %xDESCEXTRA%
@@ -82,3 +81,4 @@ goto endScript
 
 ::================================================================================================= End Script
 :endScript
+pause
