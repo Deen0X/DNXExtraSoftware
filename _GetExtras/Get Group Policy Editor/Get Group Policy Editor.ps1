25b73ef23640117a626972479241edb66f6823f5
Deen0X <deen0x.eol@gmail.com>
2023-12-15 15:37:14 +0100

    Update Get Group Policy Editor.ps1

--- _GetExtras/Get Group Policy Editor/Get Group Policy Editor.ps1
+++ _GetExtras/Get Group Policy Editor/Get Group Policy Editor.ps1
@@ -1,4 +1,10 @@
-Get-ChildItem @(
-    "C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package*.mum",
-    "C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package*.mum"
-) | ForEach-Object { dism.exe /online /norestart /add-package:"$_" }
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >List.txt 
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>List.txt 
+
+for /f %%i in ('findstr /i . List.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i" 
+
+
+#Get-ChildItem @(
+#    "C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package*.mum",
+#    "C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package*.mum"
+#) | ForEach-Object { dism.exe /online /norestart /add-package:"$_" }
