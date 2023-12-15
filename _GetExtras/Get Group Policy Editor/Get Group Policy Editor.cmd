23efde06303f12c25a4b1fd0b0f7fbd594ecca02
Deen0X <deen0x.eol@gmail.com>
2023-12-15 14:56:25 +0100

    Update Get Group Policy Editor.cmd

--- _GetExtras/Get Group Policy Editor/Get Group Policy Editor.cmd
+++ _GetExtras/Get Group Policy Editor/Get Group Policy Editor.cmd
@@ -6,7 +6,7 @@ setlocal enableextensions disabledelayedexpansion
 
 :: ALL scripts must start with "GET " text
 ::################################################################################################# Config variable for checking if Extra is installed
-set "C:\Windows\System32\gpedit.msc"
+set "checkEXTRA=C:\Windows\System32\gpedit.msc"
 ::#################################################################################################
 set xDESCEXTRA=Chocolatey is a machine-level, command-line package manager and installer for software on Microsoft Windows. It uses the NuGet packaging infrastructure and Windows PowerShell to simplify the process of downloading and installing software.
 ::#################################################################################################
