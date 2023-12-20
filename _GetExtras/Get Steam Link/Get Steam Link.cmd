8b6ac23db184997c6547c047f446e8eaa453da17
Deen0X <deen0x.eol@gmail.com>
2023-12-20 17:37:22 +0100

    Add files via upload

---
+++ _GetExtras/Get Steam Link/Get Steam Link.cmd
@@ -0,0 +1,97 @@
+@echo off
+cd /d "%~dp0"
+pushd "%~dp0"
+set mySN=%~n0
+setlocal enableextensions disabledelayedexpansion
+
+:: ALL scripts must start with "GET " text
+::################################################################################################# Config variable for checking if Extra is installed
+set "checkEXTRA=C:\Program Files (x86)\Steam Link\SteamLink.exe"
+::#################################################################################################
+set xDESCEXTRA=Play your Steam games on your mobile, tablet, TV, VR headset or other PC using Steam Link to stream them from your computer. With optimized control schemes for hundreds of the biggest titles, you can now access and play games from your PC while on the couch or on the go.
+::#################################################################################################
+set xSOURCE=https://store.steampowered.com/app/353380/Steam_Link/
+::#################################################################################################
+set xDownload=https://media.steampowered.com/steamlink/windows/latest/SteamLink.zip
+::#################################################################################################
+
+if "_%1"=="_/GETEXTRA" goto getExtra
+if "_%1"=="_/ADDMENU" goto addEntry2
+goto addEntry
+
+
+:addEntry
+::================================================================================================= Check if EXTRA exist
+echo "%checkEXTRA%"
+if exist "%checkEXTRA%" goto removeEntry
+::------------------------------------------------------------------------------------------------- Add Entry if not exist
+:addEntry2
+echo --- addEntry
+set myP=%~dp0
+set myP=%myP:~0,-1%
+set "myICO=C:\DNXSoftware\Extras\_GetExtras\DNXEXTRAS.ico"
+if exist "%~dpn0.ico" set "myICO=%~dpn0.ico"
+call "C:\DNXSoftware\Extras\_GetExtras\createlnk.cmd" "%~dpnx0" "%myP%" "%mySN%" "/GETEXTRA" "%myICO%"
+cd %myP%
+if not exist "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_Extra Software\" mkdir "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_Extra Software\"
+move /Y "%~dpn0.lnk" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_Extra Software\" >nul
+goto endScript
+
+::------------------------------------------------------------------------------------------------- Remove Entry if exist
+:removeEntry
+echo --- removeEntry
+if exist "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_Extra Software\%mySN%.lnk" del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_Extra Software\%mySN%.lnk" >nul
+goto endScript
+
+::================================================================================================= Start Main Script for Get EXTRA
+:getExtra
+echo --- getExtra
+
+::================================================================================================= Check for permissions
+:getAdminPriv
+::>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+::REM --> If error flag set, we do not have admin.
+::if '%errorlevel%' NEQ '0' (
+::    echo Requesting administrative privileges...
+::    goto UACPrompt
+::) else ( goto gotAdmin )
+::
+:::UACPrompt
+::    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\\getadmin.vbs"
+::    set params = %*:"="
+::    ::echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+::    echo UAC.ShellExecute "cmd.exe", "/c  %~s0 %1 %2 %3", "", "runas", 1 >> "%temp%\\getadmin.vbs"
+::
+::    "%temp%\getadmin.vbs"
+::    del "%temp%\\getadmin.vbs"
+::    exit /B
+::::================================================================================================= end checking admin
+:::gotAdmin
+
+cls
+echo APP Name    : %mySN%
+echo Description : %xDESCEXTRA%
+echo .
+echo Source      : %xSOURCE%
+echo --------------------------------------------------------------------------
+echo press any key to continue the installation. (close this windows for cancel)
+pause
+echo Start process for installing %mySN:~4%
+
+set myApp=SteamLink
+wget -O%myApp%.zip %xDownload%
+C:\DNXSoftware\Extras\_GetExtras\7z x -y %myApp%.Zip
+Setup.exe /quiet
+::copy "Get Chiaki Playstation Streaming.ico" "C:\DNXSoftware\Extras\Chiaki\Chiaki.ico"
+::call "C:\DNXSoftware\Extras\_GetExtras\createlnk.cmd" "C:\DNXSoftware\Extras\Chiaki\Chiaki.exe" "C:\DNXSoftware\Extras\Chiaki" "%mySN:~4%" "/GETEXTRA" "C:\DNXSoftware\Extras\Chiaki\Chiaki.ico"
+::move "C:\DNXSoftware\Extras\Chiaki\%mySN:~4%.lnk" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_DNXSoftware\"
+
+
+::------------------------------------------------------------------------------------------------- Remove Entry Extra was installed
+echo %checkEXTRA%
+if exist "%checkEXTRA%" goto removeEntry
+goto endScript
+
+
+::================================================================================================= End Script
+:endScript
