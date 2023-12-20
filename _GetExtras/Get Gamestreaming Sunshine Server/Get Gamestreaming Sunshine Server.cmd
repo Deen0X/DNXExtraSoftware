d57bd74c9f1ec13ab348eed9e8f42454e9926318
Deen0X <deen0x.eol@gmail.com>
2023-12-20 11:48:07 +0100

    Update Get Gamestreaming Sunshine Server.cmd

--- _GetExtras/Get Gamestreaming Sunshine Server/Get Gamestreaming Sunshine Server.cmd
+++ _GetExtras/Get Gamestreaming Sunshine Server/Get Gamestreaming Sunshine Server.cmd
@@ -16,6 +16,7 @@ set xDownload=https://github.com/LizardByte/Sunshine/releases/download/v0.21.0/s
 ::#################################################################################################
 
 if "_%1"=="_/GETEXTRA" goto getExtra
+if "_%1"=="_/ADDMENU" goto addEntry2
 goto addEntry
 
 
@@ -24,6 +25,7 @@ goto addEntry
 echo "%checkEXTRA%"
 if exist "%checkEXTRA%" goto removeEntry
 ::------------------------------------------------------------------------------------------------- Add Entry if not exist
+:addEntry2
 echo --- addEntry
 set myP=%~dp0
 set myP=%myP:~0,-1%
