a2c174078d439f7a5432b533610bd8588ec3b6a5
Deen0X <deen0x.eol@gmail.com>
2023-12-22 17:03:21 +0100

    Update Get Gamestreaming Moonlight.cmd

--- _GetExtras/Get Gamestreaming Moonlight/Get Gamestreaming Moonlight.cmd
+++ _GetExtras/Get Gamestreaming Moonlight/Get Gamestreaming Moonlight.cmd
@@ -47,27 +47,6 @@ goto endScript
 :getExtra
 echo --- getExtra
 
-::================================================================================================= Check for permissions
-:getAdminPriv
-::>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-::REM --> If error flag set, we do not have admin.
-::if '%errorlevel%' NEQ '0' (
-::    echo Requesting administrative privileges...
-::    goto UACPrompt
-::) else ( goto gotAdmin )
-::
-:::UACPrompt
-::    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\\getadmin.vbs"
-::    set params = %*:"="
-::    ::echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-::    echo UAC.ShellExecute "cmd.exe", "/c  %~s0 %1 %2 %3", "", "runas", 1 >> "%temp%\\getadmin.vbs"
-::
-::    "%temp%\getadmin.vbs"
-::    del "%temp%\\getadmin.vbs"
-::    exit /B
-::::================================================================================================= end checking admin
-:::gotAdmin
-
 cls
 echo APP Name    : %mySN%
 echo Description : %xDESCEXTRA%
@@ -85,7 +64,7 @@ echo Start process for installing %mySN:~4%
 
 wget -OSetup.exe %xDownload%
 Setup.exe /install /passive /norestart
-
+start "%mySN:~4%" "%checkEXTRA%"
 
 ::------------------------------------------------------------------------------------------------- Remove Entry Extra was installed
 echo %checkEXTRA%
