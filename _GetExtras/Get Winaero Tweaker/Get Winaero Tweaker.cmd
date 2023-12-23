12014d0d338809b499359e8f90244f1516d55282
Deen0X <deen0x.eol@gmail.com>
2023-12-23 19:50:38 +0100

    Add files via upload

---
+++ _GetExtras/Get Winaero Tweaker/Get Winaero Tweaker.cmd
@@ -0,0 +1,98 @@
+@echo off
+cd /d "%~dp0"
+pushd "%~dp0"
+set mySN=%~n0
+setlocal enableextensions disabledelayedexpansion
+
+:: ALL scripts must start with "GET " text
+::################################################################################################# Config variable for checking if Extra is installed
+set "checkEXTRA=1C:\Program Files\Winaero Tweaker\WinaeroTweaker.exe"
+::#################################################################################################
+set xDESCEXTRA= It is an all-in-one application that comes with dozens of options for fine-grained tuning of various Windows settings and features.
+::#################################################################################################
+set xSOURCE=https://winaerotweaker.com/
+::#################################################################################################
+set xDownload=https://winaerotweaker.com/download/winaerotweaker.zip
+::#################################################################################################
+set "auxNameExt=Winaerotweaker.zip"
+set "auxName=WinaeroTweaker-1.55.0.0-setup.exe"
+
+if "_%1"=="_/GETEXTRA" goto getExtra
+if "_%1"=="_/ADDMENU" goto addEntry2
+goto addEntry
+
+
+:addEntry
+::================================================================================================= Check if EXTRA exist
+echo "%checkEXTRA%"
+if exist "%checkEXTRA%" goto removeEntry
+::------------------------------------------------------------------------------------------------- Add Entry if not exist
+:addEntry2
+echo --- addEntry
+set myP=%~dp0
+set myP=%myP:~0,-1%
+set "myICO=C:\DNXSoftware\Extras\_GetExtras\DNXEXTRAS.ico"
+if exist "%~dpn0.ico" set "myICO=%~dpn0.ico"
+call "C:\DNXSoftware\Extras\_GetExtras\createlnk.cmd" "%~dpnx0" "%myP%" "%mySN%" "/GETEXTRA" "%myICO%"
+cd %myP%
+if not exist "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_Extra Software\" mkdir "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_Extra Software\"
+move /Y "%~dpn0.lnk" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_Extra Software\" >nul
+goto endScript
+
+::------------------------------------------------------------------------------------------------- Remove Entry if exist
+:removeEntry
+echo --- removeEntry
+if exist "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_Extra Software\%mySN%.lnk" del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_Extra Software\%mySN%.lnk" >nul
+goto endScript
+
+::================================================================================================= Start Main Script for Get EXTRA
+:getExtra
+echo --- getExtra
+
+::================================================================================================= Check for permissions
+:getAdminPriv
+::>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+::REM --> If error flag set, we do not have admin.
+::if '%errorlevel%' NEQ '0' (
+::    echo Requesting administrative privileges...
+::    goto UACPrompt
+::) else ( goto gotAdmin )
+::
+:::UACPrompt
+::    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\\getadmin.vbs"
+::    set params = %*:"="
+::    ::echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+::    echo UAC.ShellExecute "cmd.exe", "/c  %~s0 %1 %2 %3", "", "runas", 1 >> "%temp%\\getadmin.vbs"
+::
+::    "%temp%\getadmin.vbs"
+::    del "%temp%\\getadmin.vbs"
+::    exit /B
+::::================================================================================================= end checking admin
+:::gotAdmin
+
+cls
+echo APP Name    : %mySN%
+echo Description : %xDESCEXTRA%
+echo .
+echo Source      : %xSOURCE%
+echo --------------------------------------------------------------------------
+echo press any key to continue the installation. (close this windows for cancel)
+pause
+echo Start process for installing %mySN:~4%
+
+::wget "%mySN:~4%" -O%auxNameExt% %xDownload%
+C:\DNXSoftware\Extras\_GetExtras\7z x -y -OAuxExt %auxNameExt%
+start "%mySN:~4%" ".\AuxExt\%auxName%" /SP- /VERYSILENT
+PAUSE
+::call "C:\DNXSoftware\Extras\_GetExtras\createlnk.cmd" "C:\DNXSoftware\Extras\%auxName%\%auxNameExt%" "C:\DNXSoftware\Extras\%auxName%" "%mySN:~4%" "/GETEXTRA" "C:\DNXSoftware\Extras\%auxName%\%auxNameExt%"
+::move "C:\DNXSoftware\Extras\%auxName%\%mySN:~4%.lnk" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\_DNXSoftware\"
+
+
+::------------------------------------------------------------------------------------------------- Remove Entry Extra was installed
+echo %checkEXTRA%
+if exist "%checkEXTRA%" goto removeEntry
+goto endScript
+
+
+::================================================================================================= End Script
+:endScript
