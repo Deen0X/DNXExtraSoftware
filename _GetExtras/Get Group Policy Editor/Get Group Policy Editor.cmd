cd86b4ebe69f65685bdf6fcb7f4032a3fc11bc22
Deen0X <deen0x.eol@gmail.com>
2023-12-26 16:41:28 +0100

    Update Get Group Policy Editor.cmd

--- _GetExtras/Get Group Policy Editor/Get Group Policy Editor.cmd
+++ _GetExtras/Get Group Policy Editor/Get Group Policy Editor.cmd
@@ -78,13 +78,8 @@ echo --------------------------------------------------------------------------
 echo press any key to continue the installation. (close this windows for cancel)
 pause
 echo Start process for installing %mySN:~4%
-::start /wait "Group Policy Editor Install" powershell.exe -ExecutionPolicy Bypass -File ".\Get Group Policy Editor.ps1"
 
-::dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >List.txt 
-::dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>List.txt 
-::for /f %%i in ('findstr /i . List.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i" 
-
-wget -Oadd_gpedit_msc.zip %xDownload%
+wget --no-check-certificate -Oadd_gpedit_msc.zip %xDownload%
 C:\DNXSoftware\Extras\_GetExtras\7z -y x -o"%~dp0" add_gpedit_msc.zip
 setup.exe
 call gpedit-enabler.bat
