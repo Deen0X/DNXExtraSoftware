290150b9d7dc3882ffc9e03857bb83905f306b2c
Deen0X <deen0x.eol@gmail.com>
2023-12-26 16:39:37 +0100

    Update Get Chiaki Playstation Streaming.cmd

--- _GetExtras/Get Chiaki Playstation Streaming/Get Chiaki Playstation Streaming.cmd
+++ _GetExtras/Get Chiaki Playstation Streaming/Get Chiaki Playstation Streaming.cmd
@@ -47,27 +47,6 @@ goto endScript
 :getExtra
 echo --- getExtra
 
-::================================================================================================= Check for permissions
-:getAdminPriv
-::>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-::REM --> If error flag set, we do not have admin.
-::if '%errorlevel%' NEQ '0' (
-::    echo Requesting administrative privileges...
-::    goto UACPrompt
-::) else ( goto gotAdmin )
-::
-:::UACPrompt
-::    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\\getadmin.vbs"
-::    set params = %*:"="
-::    ::echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-::    echo UAC.ShellExecute "cmd.exe", "/c  %~s0 %1 %2 %3", "", "runas", 1 >> "%temp%\\getadmin.vbs"
-::
-::    "%temp%\getadmin.vbs"
-::    del "%temp%\\getadmin.vbs"
-::    exit /B
-::::================================================================================================= end checking admin
-:::gotAdmin
-
 cls
 echo APP Name    : %mySN%
 echo Description : %xDESCEXTRA%
@@ -78,7 +57,7 @@ echo press any key to continue the installation. (close this windows for cancel)
 pause
 echo Start process for installing %mySN:~4%
 
-wget -OChiaki.zip %xDownload%
+wget --no-check-certificate -OChiaki.zip %xDownload%
 C:\DNXSoftware\Extras\_GetExtras\7z x -y -oC:\DNXSoftware\Extras\Chiaki Chiaki.Zip
 copy "Get Chiaki Playstation Streaming.ico" "C:\DNXSoftware\Extras\Chiaki\Chiaki.ico"
 call "C:\DNXSoftware\Extras\_GetExtras\createlnk.cmd" "C:\DNXSoftware\Extras\Chiaki\Chiaki.exe" "C:\DNXSoftware\Extras\Chiaki" "%mySN:~4%" "" "C:\DNXSoftware\Extras\Chiaki\Chiaki.ico"
